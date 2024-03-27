4f183edfdf581c1cd511fda55a943fb7e5fbb825
vbratsyun <vbratsyun@yandex.ru>
2024-03-27 21:07:23 +0300

    добавлена фавиконка

--- build/js/script.js
+++ build/js/script.js
@@ -1,8 +1,10 @@
-      const navMain = document.querySelector(".page-nav");
-      const navToggle = document.querySelector(".page-nav__toggle");
+const navMain = document.querySelector(".page-nav");
+const navToggle = document.querySelector(".page-nav__toggle");
+const example = document.querySelector(".example__before-after");
+const exampleSlider = document.querySelector(".example__slider");
+const exampleControl = document.querySelector(".example__range-scale-control");
 
-      navMain.classList.remove("page-nav--nojs");
-
+navMain.classList.remove("page-nav--nojs");
 
 const onNavToggleClick = () => {
   if (navMain.classList.contains("page-nav--closed")) {
@@ -12,40 +14,30 @@ const onNavToggleClick = () => {
     navMain.classList.add("page-nav--closed");
     navMain.classList.remove("page-nav--opened");
   }
-}
-      navToggle.addEventListener("click", onNavToggleClick);
+};
 
-      const example = document.querySelector(".example__before-after");
-      const exampleSlider = document.querySelector(".example__slider");
-      const exampleControl = document.querySelector(
-        ".example__range-scale-control"
-      );
+navToggle.addEventListener("click", onNavToggleClick);
 
-      const onExampleClick = (event) => {
-        const imageAfter = exampleSlider.querySelector(
-          ".example__image--after"
-        );
-        const imageBefore = exampleSlider.querySelector(
-          ".example__image--before"
-        );
 
-        if (event.target.matches(".example__button--after")) {
-          imageAfter.classList.remove("hidden");
-          imageBefore.classList.add("hidden");
-          exampleControl.classList.add("example__range-scale-control--after");
-        } else {
-          imageAfter.classList.add("hidden");
-          imageBefore.classList.remove("hidden");
-          exampleControl.classList.remove(
-            "example__range-scale-control--after"
-          );
-        }
-      };
+const onExampleClick = (event) => {
+  const imageAfter = exampleSlider.querySelector(".example__image--after");
+  const imageBefore = exampleSlider.querySelector(".example__image--before");
 
-      example.addEventListener("click", onExampleClick);
+  if (event.target.matches(".example__button--after")) {
+    imageAfter.classList.remove("hidden");
+    imageBefore.classList.add("hidden");
+    exampleControl.classList.add("example__range-scale-control--after");
+  } else {
+    imageAfter.classList.add("hidden");
+    imageBefore.classList.remove("hidden");
+    exampleControl.classList.remove("example__range-scale-control--after");
+  }
+};
 
-      const processNumbers = document.querySelectorAll(".process__number");
-      processNumbers.forEach((num, index) => {
-        num.textContent = index + 1;
-        num.classList.add(".process__number");
-      });
+example.addEventListener("click", onExampleClick);
+
+const processNumbers = document.querySelectorAll(".process__number");
+processNumbers.forEach((num, index) => {
+  num.textContent = index + 1;
+  num.classList.add(".process__number");
+});
